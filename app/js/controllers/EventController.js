3b1ca01c56ef79ef51795b8cee8eed3a46a0395b
decoy31 <decoy31@hotmail.com>
2013-06-01 15:39:37 -0700

    Added event details and controller. Modified node server code to allow for json file data manipulation.

---
+++ app/js/controllers/EventController.js
@@ -0,0 +1,28 @@
+'use strict';
+
+eventsApp.controller('EventController',
+    function EventController ($scope) {
+        $scope.event = {
+            name: 'Angular Boot Camp',
+            date: '1/1/2013',
+            time: '10:30 am',
+            location: {
+                address: 'Google Headquarters',
+                city: 'Mountain View',
+                state: 'CA'
+            },
+            imageUrl: '/img/angularjs-logo.png',
+            sessions: [
+                {
+                    name: 'Directives Masterclass'
+                },
+                {
+                    name: 'Scopes for fun and profit'
+                },
+                {
+                    name: 'Well Behaved Controllers'
+                }
+            ]
+        };
+    }
+);
