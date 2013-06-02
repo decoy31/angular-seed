51a85462127c4d621b45f2166f47d730956dac9c
decoy31 <decoy31@hotmail.com>
2013-06-01 23:36:44 -0700

    Using $q promises instead of callbacks to get event data.

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -4,8 +4,12 @@ eventsApp.controller('EventController',
     function EventController ($scope, eventData) {
 
         $scope.sortorder = 'name';
-        eventData.getEvent(function (event) {
-            $scope.event = event;
+        $scope.event = eventData.getEvent(1);
+
+        $scope.event.then(function (event) {
+            console.log(event);
+        }, function (status) {
+            console.warn(status);
         });
 
         $scope.upVoteSession = function (session) {
--- app/js/services/EventData.js
+++ app/js/services/EventData.js
@@ -1,19 +1,29 @@
 'use strict';
 
-eventsApp.factory('eventData', function ($http, $log) {
+eventsApp.factory('eventData', function ($http, $q) {
     return {
-        getEvent: function (successCb) {
+        /**
+         * Gets the stored event.
+         *
+         * @returns {promise} Returns a promise object.
+         */
+        getEvent: function (id) {
+            var deferred = $q.defer();
+
             $http({
                 method: 'GET',
-                url: '/data/event/1'
+                url: '/data/event/' + id
             })
                 .success(function (data, status, headers, config) {
-                    $log.info(data, status, headers(), config);
-                    successCb(data);
+                    //$log.info(data, status, headers(), config);
+                    deferred.resolve(data);
                 })
                 .error(function (data, status, headers, config) {
-                    $log.warn(data, status, headers(), config);
+                    //$log.warn(data, status, headers(), config);
+                    deferred.reject(status);
                 });
+
+            return deferred.promise;
         }
     };
 });
