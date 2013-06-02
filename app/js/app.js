657d570c9e76a4b2bf43459f8ea1cf424e459869
decoy31 <decoy31@hotmail.com>
2013-06-02 13:11:30 -0700

    Switched to HTML5 routing.

--- app/js/app.js
+++ app/js/app.js
@@ -1,25 +1,27 @@
 'use strict';
 
 var eventsApp = angular.module('eventsApp', ['ngSanitize', 'ngResource'])
-    .config(function ($routeProvider) {
+    .config(function ($routeProvider, $locationProvider) {
         $routeProvider.when('/newEvent',
             {
-                templateUrl: 'templates/NewEvent.html',
+                templateUrl: '/templates/NewEvent.html',
                 controller: 'EditEventController'
             });
 
         $routeProvider.when('/events',
             {
-                templateUrl: 'templates/EventList.html',
+                templateUrl: '/templates/EventList.html',
                 controller: 'EventListController'
             });
 
         $routeProvider.when('/events/:eventId',
             {
-                templateUrl: 'templates/EventDetails.html',
+                templateUrl: '/templates/EventDetails.html',
                 controller: 'EventController'
             });
 
         // Default Route
         $routeProvider.otherwise({ redirectTo: '/events'});
+
+        $locationProvider.html5Mode(true);
     });
