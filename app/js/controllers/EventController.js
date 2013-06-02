dfa1426b4bf555cdb426735303980ade90769be7
decoy31 <decoy31@hotmail.com>
2013-06-02 12:20:43 -0700

    Added a $route implementation using a refresh button on the EventDetails page.

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -1,10 +1,18 @@
 'use strict';
 
 eventsApp.controller('EventController',
-    function EventController ($scope, eventData, $routeParams) {
+    function EventController ($scope, eventData, $routeParams, $route) {
         $scope.sortorder = 'name';
         //$scope.event = $route.current.locals.event;
         $scope.event = eventData.getEvent($routeParams.eventId);
+
+        // $route.current.pathParams only include parameters on the route.
+        // $route.current.params includes both query string and route params.
+        console.log($route.current.pathParams.eventId);
+
+        $scope.reload = function () {
+            $route.reload();
+        };
 
         $scope.upVoteSession = function (session) {
             session.upVoteCount++;
