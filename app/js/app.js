1d4433b2ee6b40d71ff2a20c98eb4c3da173d7c1
decoy31 <decoy31@hotmail.com>
2013-06-02 11:15:15 -0700

    Began to add routing.

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,10 @@
 'use strict';
 
-var eventsApp = angular.module('eventsApp', ['ngSanitize', 'ngResource', 'ngCookies'])
-    .factory('myCache', function ($cacheFactory) {
-        return $cacheFactory('myCache', {capacity: 3});
+var eventsApp = angular.module('eventsApp', ['ngSanitize', 'ngResource'])
+    .config(function ($routeProvider) {
+        $routeProvider.when('/NewEvent',
+            {
+                templateUrl: 'templates/newEvent.html',
+                controller: 'EditEventController'
+            });
     });
